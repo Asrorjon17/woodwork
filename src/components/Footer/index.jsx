8a84cd547a8074f2c38edee07c9ed46b168c19f0
Asrorjon17 <asrorjonrahimjonov17@gmail.com>
2022-05-26 16:03:26 +0500

    Asrorjon tugatdi

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,106 +1,116 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
-import React from "react";
-import woo from "../../assets/img/w00.webp";
-import Map from "../Map";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
-export default () => {
-	return (
-		<>
-			<Box sx={{ bgcolor: "black" }}>
-				<Container maxWidth="lg">
-					<Box sx={{ color: "white", pt: "5%" }}>
-						<Grid container>
-							<Grid item xs={12} sm={12} md={6} lg={6}>
-								<Box
-									sx={{
-										width: "100%",
-										display: "flex",
-										flexDirection: "column",
-										gap: 3,
-									}}
-								>
-									<Box>
-										<img src={woo} alt="" />
-									</Box>
-									<Typography>
-										Consectetur adipiscing elit, sed do eiusmod tempor
-										incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida.
-									</Typography>
-									<Typography>
-										<span style={{ color: "#A0A526" }}>Address:</span> 450
-										Strand, Charing Cross, London
-										<br />
-										WC2R 0RG, US
-									</Typography>
-									<Typography>
-										<span style={{ color: "#A0A526" }}>Phone:</span> +44 20 7930
-										8205 - +44 20 7839 2323
-									</Typography>
-									<Typography>
-										<span style={{ color: "#A0A526" }}>Email: </span>{" "}
-										Support@woodworking.com
-									</Typography>
-								</Box>
-							</Grid>
-							<Grid item xs={12} sm={12} md={6} lg={6}>
-								<Map />
-							</Grid>
-						</Grid>
-						<Box sx={{ my: "5%" }} />
-						<hr />
-						<Box sx={{ my: "3%" }} />
+import React from 'react'
+import { Box, Grid, Container, Typography, Input, Button, ButtonGroup } from '@mui/material'
 
-						<Grid container>
-							<Grid
-								item
-								xs={12}
-								lg={6}
-								sx={{ display: "flex", justifyContent: "center" }}
-							>
-								<Typography
-									sx={{
-										display: "flex",
-										alignItems: "center",
-										flexWrap: "wrap",
-									}}
-								>
-									Copyright ©2022 All rights reserved | This template is made
-									with
-								</Typography>
-							</Grid>
-							<Grid
-								item
-								xs={12}
-								lg={3}
-								sx={{ display: "flex", justifyContent: "center" }}
-							>
-								<Typography sx={{ display: "flex", alignItems: "center" }}>
-									<FavoriteIcon sx={{ color: "#A0A526" }} /> by Colorlib
-								</Typography>
-							</Grid>
-							<Grid
-								item
-								xs={12}
-								lg={3}
-								sx={{ display: "flex", justifyContent: "center" }}
-							>
-								<Typography
-									sx={{ display: "flex", alignItems: "center", gap: 1.5 }}
-								>
-									Follow us:
-									<FacebookIcon /> <TwitterIcon /> <InstagramIcon />
-								</Typography>
-							</Grid>
-							<Box sx={{ my: "2%" }} />
-						</Grid>
-					</Box>
-				</Container>
-			</Box>
-		</>
-	);
-};
+// import './index.css'
+
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import GoogleIcon from '@mui/icons-material/Google';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+
+// import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
+// import AddchartIcon from '@mui/icons-material/Addchart';
+// import BurstModeIcon from '@mui/icons-material/BurstMode';
+// import CallToActionIcon from '@mui/icons-material/CallToAction';
+
+
+export default () => {
+  return (
+    <Box lighter sx={{
+      bgcolor: '#F0F0F0',
+      width: '100%',
+      color: '#888888',
+      height: '100%'
+      // fontWeight: 'lighter'
+    }}>
+      <Container >
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          aliginItems: 'center',
+          pt: 10,
+        }}>
+          <Grid container spacing={1}>
+            <Grid md={4} xs={6}>
+              <Box sx={{ width: "90%" }}>
+                <Typography sx={{ fontWeight: 'bold', color: '#000' }} > GET IN TOUCH</Typography>
+                <br />
+                <Typography>Any questions? Let us  8th floor, 379
+                  Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+                </Typography>
+
+                <FacebookOutlinedIcon />
+                <TwitterIcon />
+                <PinterestIcon />
+                <GoogleIcon />
+                <InstagramIcon />
+              </Box>
+            </Grid>
+            <Grid md={2} xs={12} >
+              <Box sx={{ gapTop: 4 }}>
+                <Typography sx={{ fontWeight: 'bold', color: '#000' }} > CATEGORIES</Typography>
+                <br />
+                <Typography>Men</Typography>
+                <Typography>Women</Typography>
+                <Typography>Dresses</Typography>
+                <Typography>Sunglasses</Typography>
+              </Box>
+            </Grid>
+            <Grid md={2} xs={12}>
+              <Box>
+                <Typography sx={{ fontWeight: 'bold', color: '#000' }}>LINCKS</Typography>
+                <br />
+                <Typography>Search</Typography>
+                <Typography>About Us</Typography>
+                <Typography>Contact Us</Typography>
+                <Typography>Returns</Typography>
+              </Box>
+            </Grid>
+            <Grid md={2} xs={12}>
+              <Box>
+                <Typography sx={{ fontWeight: 'bold', color: '#000' }} > HELP</Typography>
+                <br />
+                <Typography>Track Order</Typography>
+                <Typography>Returns</Typography>
+                <Typography>Shipping</Typography>
+                <Typography>FAQs</Typography>
+              </Box>
+            </Grid >
+            <Grid md={2} xs={12}>
+              <Box sx={{ gapTop: 4 }}>
+                <Typography sx={{ fontWeight: 'bold', color: '#000' }} > NEWSLETTER</Typography>
+                <br />
+                <Input sx={{ width: 'auto' }} type='text' placeholder='Email Address'></Input>
+                <br />
+                <br />
+                <Button sx={{
+                  bgcolor: '#000',
+                  color: '#fff',
+                  borderRadius: 50,
+                  width: '80%',
+                  gapTop: 4,
+                  // hoder: {
+                  //   bgcolor: '#EB4C42'
+                  // }
+
+                }}>SUBSCRIBE</Button>
+              </Box>
+            </Grid >
+          </Grid >
+        </Box >
+      </Container>
+      <Grid md={12} xs={12}>
+        {/* <Box sx={{ alignItems: 'center' }}>
+          <FeaturedPlayListIcon />
+          <AddchartIcon />
+          <BurstModeIcon />
+          <CallToActionIcon />
+        </Box> */}
+        <Typography sx={{ textAlign: 'center', pt: 17 }}>Copyright © 2018 MassTechnologist.com. All rights reserved.</Typography>
+      </Grid>
+    </Box >
+  )
+}
+
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,120 +1,315 @@
-import { Box, Button, Container, Grid, Link, Typography } from "@mui/material";
+import {
+	Box,
+	Button,
+	Container,
+	Grid,
+	Input,
+	InputBase,
+	Menu,
+	MenuItem,
+	TextField,
+	Typography,
+} from "@mui/material";
+import { FaFacebookF, FaGooglePlusG, FaPinterest } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
 import React from "react";
-import navrasm from "../../assets/img/download (1).webp";
-import Buttone from "../Button";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+
 import MenuIcon from "@mui/icons-material/Menu";
-const Nav = () => {
+const SwipeableTemporaryDrawer = () => {
+	const [state, setState] = React.useState({
+		top: false,
+	});
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+			event &&
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		setState({ ...state, [anchor]: open });
+	};
+
+	const list = (anchor) => (
+		<Box
+			sx={{
+				width: anchor === "left" || anchor === "bottom" ? "auto" : 250,
+			}}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<NavbarBox />
+		</Box>
+	);
+
 	return (
 		<div>
-			<Box sx={{ width: "100%" }}>
-				<Container maxWidth="lg">
-					<Box
-						sx={{
-							display: "flex",
-							justifyContent: "space-between",
-							width: "100%",
-							paddingY: "2%",
-							// border: 1,
-						}}
-						// className="border-2"
+			{["top"].map((anchor) => (
+				<Box key={anchor} sx={{ py: "10%" }}>
+					<Button
+						onClick={toggleDrawer(anchor, true)}
+						sx={{ color: "black", border: 1, }}
 					>
-						<Grid container spacing={0}>
-							<Grid item lg={4} xs={6} sm={3} md={3} container spacing={0}>
-								<Box sx={{ display: "flex", alignItems: "center" }}>
-									<img src={navrasm} alt="" width="100%" />
-								</Box>
-							</Grid>
-							<Grid item lg={8} xs={6} sm={9} md={9} container spacing={0}>
-								<Box
-									sx={{
-										display: "flex",
-										justifyContent: "center",
-										width: "100%",
-										// border: 1,
-									}}
-									// className="border-2"
-								>
-									<Box
-										sx={{
-											width: "100%",
-											// display: "flex",
-											justifyContent: "space-around",
-											alignItems: "center",
-											px: "2%",
-											display: { md: "flex", sm: "flex", xs: "none" },
-										}}
-									>
-										{[
-											"HOME",
-											"ABOUT",
-											"SERVICES",
-											"PAGES",
-											"BLOG",
-											"CONTACTS",
-										].map((item) => (
-											<Link
-												href="#"
-												underline="none"
-												fontSize="90%"
-												sx={{
-													color: "black",
-													borderBottom: 3,
-													py: "1.5%",
-													borderColor: "white",
-
-													":hover": {
-														borderBottomColor: "#BFB332",
-													},
-													":active": {
-														borderBottomColor: "#BFB332",
-														transform: "2s",
-													},
-												}}
-											>
-												{item}
-											</Link>
-										))}
-
-										<Button
-											sx={{
-												display: { md: "flex", xs: "none", sm: "none" },
-												bgcolor: "#BFB332",
-												borderRadius: 0,
-												px: "5%",
-												fontWeight: "bold",
-												py: "1%",
-												":hover": {
-													bgcolor: "#BFB332",
-												},
-											}}
-											variant="contained"
-											fontSize="90%"
-										>
-											GET A QUOTE
-										</Button>
-									</Box>
-									<Box
-										sx={{
-											display: { md: "none", xs: " flex", sm: " none" },
-											width: "100%",
-											justifyContent: "end",
-											marginLeft: "30vw",
-											alignItems: "center",
-										}}
-									>
-										<MenuIcon
-											fontSize="large"
-											sx={{ border: 1, m: "7%", borderRadius: 1 }}
-										/>
-									</Box>
-								</Box>
-							</Grid>
-						</Grid>
-					</Box>
-				</Container>
-			</Box>
+						<MenuIcon />
+					</Button>
+					<SwipeableDrawer
+						anchor={anchor}
+						open={state[anchor]}
+						onClose={toggleDrawer(anchor, false)}
+						onOpen={toggleDrawer(anchor, true)}
+					>
+						{list(anchor)}
+					</SwipeableDrawer>
+				</Box>
+			))}
 		</div>
 	);
 };
+const NavbarBox = () => {
+	const sty = {
+		px: "1%",
+		py: "1%",
+		borderBottom: "3px solid #fff",
 
-export default Nav;
+		":hover": { borderBottom: "3px solid #C59E47", bgcolor: "white" },
+		borderRadius: 0,
+		color: "black",
+	};
+	return (
+		<Grid
+			container
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+			}}
+		>
+			<Grid
+				xl={8}
+				lg={8}
+				md={8}
+				xs={0}
+				sx={{
+					display: "flex",
+					gap: 2,
+					flexDirection: "column",
+					py: "1%",
+
+					px: "3%",
+					justifyContent: "space-between",
+				}}
+			>
+				<Button variant="text" sx={sty}>
+					HOME
+				</Button>
+				<Button variant="text" sx={sty}>
+					Shop
+				</Button>
+				<Button variant="text" sx={sty}>
+					Sale
+				</Button>
+				<Button variant="text" sx={sty}>
+					Features
+				</Button>
+				<Button variant="text" sx={sty}>
+					About
+				</Button>
+				<Button variant="text" sx={sty}>
+					Contact
+				</Button>
+			</Grid>
+		</Grid>
+	);
+};
+export default () => {
+	const sty = {
+		px: "1%",
+		py: "1%",
+		borderBottom: "3px solid #fff",
+
+		":hover": { borderBottom: "3px solid #C59E47", bgcolor: "white" },
+		borderRadius: 0,
+		color: "black",
+	};
+	return (
+		<>
+			<Grid
+				container
+				xl={12}
+				lg={12}
+				md={12}
+				sx={{ width: "100%", bgcolor: "black" }}
+			>
+				<Container maxWidth="xl" sx={{ bgcolor: '#F0F0F0', color: '#8888', }}>
+					<Grid
+						container
+						sx={{
+							maxWidth: "100%",
+							// minWidth: "450px",
+							// px: "9.5%",
+							// color: "white",
+							display: "flex",
+							justifyContent: "center",
+						}}
+					>
+						<Grid
+							item
+							xl={10}
+							lg={10}
+							md={10}
+							xs={12}
+							sx={{
+								px: "1.3%",
+								py: "0.4%",
+								display: "flex",
+								justifyContent: "space-around",
+							}}
+						>
+							<Grid item xl={4} lg={4} md={4} xs={4}>
+								<Box sx={{ display: "flex", gap: "0.8vw", pb: "0.2%", }}>
+									<AiOutlineTwitter style={{ fontSize: "22px" }} />
+									<FaGooglePlusG style={{ fontSize: "22px" }} />
+									<FaPinterest style={{ height: "22px" }} />
+								</Box>
+							</Grid>
+
+							<Grid
+								item
+								xl={4}
+								lg={4}
+								md={4}
+								xs={4}
+								sx={{ display: "flex", justifyContent: "center" }}
+							>
+								<Typography fontSize="80%" sx={{ fontFamily: "sans-serif" }}>
+									Free shipping for standard order over $100
+								</Typography>
+							</Grid>
+							<Grid
+								item
+								xl={4}
+								lg={4}
+								md={4}
+								xs={4}
+								sx={{
+									display: "flex",
+									justifyContent: "start",
+								}}
+							>
+								<Box
+									sx={{
+										display: "flex",
+										width: "90%",
+
+										justifyContent: "end",
+									}}
+								>
+									<Typography
+										fontSize="80%"
+										sx={{
+											fontFamily: "sans-serif",
+											":hover": { color: "#FDBE33" },
+										}}
+									>
+										LOGIN
+									</Typography>
+									<Typography fontSize="80%" sx={{ mx: "3px" }}>
+										|
+									</Typography>
+
+									<Typography
+										fontSize="80%"
+										sx={{
+											fontFamily: "sans-serif",
+											":hover": { color: "#FDBE33" },
+										}}
+									>
+										USD
+									</Typography>
+								</Box>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Container>
+				<Box sx={{ width: "100%", bgcolor: "white", px: "9.5%" }}>
+					<Container maxWidth="xl" sx={{ position: "relative" }}>
+						<Box sx={{ width: "100%" }}>
+							<Box sx={{ position: "absolute", top: 10, }}>
+								<Typography sx={{ fontWeight: 'bold', fontSize: 30 }}>Fashe</Typography>
+							</Box>
+							<Box
+								sx={{
+									display: { xs: "none", sm: "none", lg: "flex", md: "flex" },
+								}}
+							>
+								<Grid
+									container
+									sx={{
+										display: "flex",
+										justifyContent: "center",
+									}}
+								>
+									<Grid
+										xl={8}
+										lg={8}
+										md={8}
+										xs={0}
+										sx={{
+											display: "flex",
+											gap: 5,
+											py: "1%",
+
+											px: "3%",
+											justifyContent: "space-between",
+										}}
+									>
+										<Button variant="text" sx={sty}>
+											HOME
+										</Button>
+										<Button variant="text" sx={sty}>
+											Shop
+										</Button>
+										<Button variant="text" sx={sty}>
+											Sale
+										</Button>
+										<Button variant="text" sx={sty}>
+											Features
+										</Button>
+										<Button variant="text" sx={sty}>
+											About
+										</Button>
+										<Button variant="text" sx={sty} >
+											Contact
+										</Button>
+									</Grid>
+								</Grid>
+							</Box>
+							<Box
+								sx={{
+									width: "100%",
+									display: { xs: "flex", sm: "flex", lg: "none", md: "none" },
+									justifyContent: "end",
+								}}
+							>
+								<Box>
+									<SwipeableTemporaryDrawer />
+								</Box>
+							</Box>
+						</Box>
+					</Container>
+				</Box>
+			</Grid>
+		</>
+	);
+};
+
+
+
+
+
+
+
+// eng kaatta box ga 1 ci navbar displar:{md:'none', lg:'flex  '}
